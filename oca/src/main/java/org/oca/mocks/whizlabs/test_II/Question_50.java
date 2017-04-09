709313fda545f9eb5565bbc5d31b0e17ffb59622
maxJunior04 <maxleoj@gmail.com>
2017-04-09 13:58:58 -0500

    changes in my home - sunday after launch

---
+++ oca/src/main/java/org/oca/mocks/whizlabs/test_II/Question_50.java
@@ -0,0 +1,14 @@
+package org.oca.mocks.whizlabs.test_II;
+
+/**
+ * Created by mx on 9/4/2017.
+ */
+public class Question_50 {
+    public static void main(String[] args) {
+        this_could_be_awesome_function_to_the_question();
+    }
+
+    private static void this_could_be_awesome_function_to_the_question(){
+
+    }
+}
