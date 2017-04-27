9a0cbbe321932b43fce7cb37320a7accd1d4cf41
max.macalupu <max.macalupu@avantica.net>
2017-04-27 12:22:02 -0500

    changes for last test_IV

---
+++ oca/src/main/java/org/oca/mocks/whizlabs/test_IV/Question_19.java
@@ -0,0 +1,28 @@
+package org.oca.mocks.whizlabs.test_IV;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+/**
+ * Created by AVANTICA TECHNOLOGIES on 4/27/2017.
+ */
+public class Question_19 {
+
+    public static void main(String[] args) throws IOException {
+        FileVisitor fileVisitor = new FileVisitorFor_Q19();
+        Files.walkFileTree(Paths.get("E:\\data"), fileVisitor);
+    }
+
+    private static final class FileVisitorFor_Q19 extends SimpleFileVisitor<Path>{
+        PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:*.java");
+
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException{
+//            System.out.println(file.toAbsolutePath());
+            if(pathMatcher.matches(file.getFileName())){
+                System.out.println(file.toAbsolutePath());
+            }
+            return FileVisitResult.CONTINUE;
+        }
+    }
+}
